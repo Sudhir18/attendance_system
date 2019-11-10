ae3ffa5c001ff5221792e2f712f7d00dc0ad4689
Sudhir18 <sudhir.sawant0@gmail.com>
2019-11-10 17:37:38 +0530

    setup template

--- attendance_module/views.py
+++ attendance_module/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
+# auth packages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+
 # Create your views here.
+
+def login_view(request):
+    return render(request, 'attendance_module/user_login.html', {})
+
+
+def user_login(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user:
+            login(request,user)
+            return HttpResponseRedirect(reverse('attendance_system_app:user_dashboard'))
+        else:
+            return HttpResponse("Incorrect credentials")
+
+
+def user_dashboard(request):
+    context = {}
+    return render(request, 'attendance_module/dashboard.html', context=context)
